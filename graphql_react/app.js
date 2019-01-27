73474fb89bc7980723024309edbf0662795e5567
Oyomi <seayomi@gmail.com>
2019-01-27 04:07:13 +0100

    buil graph api

--- graphql_react/app.js
+++ graphql_react/app.js
@@ -1,14 +1,40 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';   
+import graphqlHttp from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json ());
 
-app.get('/', (req, res, next) => {
-  res.json('Welcome to my GraphQL API');
-});
+app.use('/graphapi', graphqlHttp({
+  schema: buildSchema(`
+
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String  
+    }
+    
+    schema {
+      query: RootQuery 
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['ALC','Omu resort'];
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName; 
+    }
+  },
+  graphiql: true,
+}));
 
 app.listen(port, () => {
   console.log(`GraphQL server is running on: ${port}`);
